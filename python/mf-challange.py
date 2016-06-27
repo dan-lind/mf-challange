228e2eaf154de13eee48a872bc1a36c5dacd76ef
Daniel Lindberg <daniel.lindberg@omegapoint.se>
2016-06-27 22:14:35 +0200

    Improved printed information for python

--- python/mf-challange.py
+++ python/mf-challange.py
@@ -12,15 +12,16 @@ with open('../buy-sell.json') as json_data:
 df = pd.DataFrame(d['data'])
 df.index = pd.to_datetime(df.quote_date)
 
-print df.close.min()
-print df.close.idxmin()
-print df.low.min()
-print df.low.idxmin()
-
-print df.close.max()
-print df.close.idxmax()
-print df.high.max()
-print df.high.idxmax()
+print ''
+print '--------- Optimal Buy/Sell for close ----------'
+print 'Buy at close price ' + df.close.min() + ' on ' + str(df.close.idxmin())
+print 'Sell at close price ' + df.close.max() + ' on ' + str(df.close.idxmax())
+print' Profit: ' + str(float(df.close.max()) - float(df.close.min())) + ' SEK per share'
+print ''
+print '--------- Optimal Buy/Sell for low/high ----------'
+print 'Buy at low price ' + df.low.min() + ' on ' + str(df.low.idxmin())
+print 'Sell at high price ' + df.high.max() + ' on ' + str(df.high.idxmax())
+print' Profit: ' + str(float(df.high.max()) - float(df.low.min())) + ' SEK per share'
 
 plt.plot_date(df.index,df.close,ls='-',marker=' ')
 plt.plot(df.close.idxmin(),float(df.close.min()), marker='o')
